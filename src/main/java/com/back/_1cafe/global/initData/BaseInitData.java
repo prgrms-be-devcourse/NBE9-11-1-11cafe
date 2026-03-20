bcb2c13772e3825478d59dfcddf55456ac5a4b3b
alison0525 <ak501636@gmail.com>
2026-03-20 16:22:58 +0900

    feat: 상품 조회 API 구현
    # Conflicts:
    #	src/main/java/com/back/_1cafe/cart/Cart.java
    #	src/main/java/com/back/_1cafe/cart/CartItem.java
    #	src/main/java/com/back/_1cafe/customer/Customer.java
    #	src/main/java/com/back/_1cafe/global/rsData/RsData.java
    #	src/main/java/com/back/_1cafe/order/Order.java
    #	src/main/java/com/back/_1cafe/order/OrderItem.java
    #	src/main/java/com/back/_1cafe/product/Product.java

--- src/main/java/com/back/_1cafe/global/initData/BaseInitData.java
+++ src/main/java/com/back/_1cafe/global/initData/BaseInitData.java
@@ -1,4 +1,29 @@
 package com.back._1cafe.global.initData;
 
-public class BaseInitData {
+import com.back._1cafe.product.Product;
+import com.back._1cafe.product.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Configuration
+public class BaseInitData implements ApplicationRunner {
+
+    private final ProductRepository productRepository;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        if (productRepository.count() == 0) {
+            productRepository.saveAll(List.of(
+                    new Product("Columbia",15000,"SINGLE_ORIGIN"),
+                    new Product("Ethiopia", 17000,"SINGLE_ORIGIN"),
+                    new Product("Brazil",13000, "BLENDED"),
+                    new Product("Kenya", 16000,  "SINGLE_ORIGIN")
+            ));
+        }
+    }
 }
--- src/main/java/com/back/_1cafe/product/ProductService.java
+++ src/main/java/com/back/_1cafe/product/ProductService.java
@@ -1,4 +1,18 @@
 package com.back._1cafe.product;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    //전체 조회
+    public List<Product> getAllProducts(){
+        return productRepository.findAll();
+    }
 }
