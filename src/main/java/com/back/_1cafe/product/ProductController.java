5c4d5d43e83f456a45231eec8a6ea5e06f870d42
alison0525 <ak501636@gmail.com>
2026-03-20 16:22:58 +0900

    feat: 상품 조회 API 구현
    # Conflicts:
    #	src/main/java/com/back/_1cafe/cart/Cart.java
    #	src/main/java/com/back/_1cafe/cart/CartItem.java
    #	src/main/java/com/back/_1cafe/customer/Customer.java
    #	src/main/java/com/back/_1cafe/global/rsData/RsData.java
    #	src/main/java/com/back/_1cafe/order/Order.java
    #	src/main/java/com/back/_1cafe/order/OrderItem.java
    #	src/main/java/com/back/_1cafe/product/Product.java

--- src/main/java/com/back/_1cafe/product/ProductController.java
+++ src/main/java/com/back/_1cafe/product/ProductController.java
@@ -1,4 +1,26 @@
 package com.back._1cafe.product;
 
+
+import com.back._1cafe.global.rsData.RsData;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/products")
 public class ProductController {
+
+    private final ProductService productService;
+
+    @GetMapping
+    public RsData<List<ProductDto>> list(){
+         List<ProductDto> productDtoList = productService.getAllProducts().stream()
+                                    .map(ProductDto::new)
+                                    .toList();
+         return new RsData<>(true, "상품 목록 조회 성공", productDtoList);
+    }
 }
