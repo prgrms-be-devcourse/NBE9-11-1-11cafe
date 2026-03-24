ee14638d05a92c698d9de8f6d3023bd25a86659b
HanJeongmok <wjdahe1845@gmail.com>
2026-03-24 15:15:15 +0900

    [Refactor]:Order Jpa부분 수정

--- src/main/java/com/back/_1cafe/order/OrderRepository.java
+++ src/main/java/com/back/_1cafe/order/OrderRepository.java
@@ -2,5 +2,5 @@ package com.back._1cafe.order;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface OrderRepository extends JpaRepository<Orders,Long> {
+public interface OrderRepository extends JpaRepository<Orders,Integer> {
 }
---
+++ src/test/java/com/back/_1cafe/cart/CartServiceTest.java
@@ -0,0 +1,4 @@
+package com.back._1cafe.cart;
+
+public class CartServiceTest {
+}
